f5c202555d9624c93fa5921e1c67257f853b03c8
juandh4cker <juandiegotorregroza@gmail.com>
2025-01-13 12:37:20 -0500

    Orden

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Login from './components/Login';
-import Register from './components/Register';
-
-import Dashboard from './components/Dashboard';
-import Blog from './components/blog';
-import DestinoDetalle from './components/DestinoDetalle';
-import PrivateRoute from './components/PrivateRoute';
-import EditDestino from './components/EditDestino';
-
-
-import Perfil from './components/Perfil';
+import Fondo from './components/useful/fondo/Fondo';
+import MenuButton from './components/useful/menuButton/MenuButton';
+import AppRoutes from './components/useful/AppRoutes';
 
 import './App.css';
 
 const App = () => {
   return (
     <Router basename="/Blog">
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route 
-          path="/blog" 
-          element={
-            <PrivateRoute>
-              <Blog />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/dashboard" 
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/destino/:id" 
-          element={
-            <PrivateRoute>
-              <DestinoDetalle />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/perfil/:id" 
-          element={
-            <PrivateRoute>
-              <Perfil />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/editar-destino/:id" 
-          element={
-            <PrivateRoute>
-              <EditDestino />
-            </PrivateRoute>
-          } 
-        />
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/dashboard" />} />
-      </Routes>
+      <AppRoutes />
+      <Fondo />
+      <MenuButton />
     </Router>
   );
 };
--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('user');
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
